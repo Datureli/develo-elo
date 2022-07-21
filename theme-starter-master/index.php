68e8aefa45ca8c80b260620dfb45f81d41fb0ceb
Datureli <soleris1@gmail.com>
2022-07-21 23:36:21 +0200

    set background color of login form

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -4,9 +4,22 @@ get_header();
 
 <?php the_content(); ?>
 
-<div class="loginpage__background">
-       <h1>dsdsdasdas</h1>     
- </div>
+<div class="loginpage__background">    
+     <div class="loginpage__logincontainer">
+        <h1>Log in</h1>
+        <p>Log in with your data that you recieved from the photographer</p>
+        <form>
+        <input type="text" placeholder="E-mail" class="input">
+        <input type="text" placeholder="Password" class="input">
+        <div class="loginpage__flexcontainer">
+        <p class="loginpage__paraghraph">forgot password?</p>
+        <button class="loginpage__button" type="submit">Log in</button>
+        </div>
+
+        </form>
+
+     </div>
+    </div>
  
 <?php
 get_footer();
