74eeed13dba229043308fc1e3057b1fb6793346d
Datureli <soleris1@gmail.com>
2022-07-23 13:18:41 +0200

    fix problems from pull request

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -1,21 +1,23 @@
 <?php
 get_header();
 ?>
-
+<section class="loginpage">
 <div class="loginpage__background">
     <div class="loginpage__container">
         <h1 class="loginpage__h1">Log in</h1>
-        <p class="loginpage_first-paragraph loginpage__align-paragraph">Log in with your data that you recieved from the photographer</p>
+        <p class="loginpage_description loginpage__align-left">Log in with your data that you recieved from the photographer</p>
         <form>
             <input type="text" placeholder="E-mail" class="loginpage__input">
             <input type="text" placeholder="Password" class="loginpage__input">
             <div class="loginpage__flexcontainer">
-                <p class="loginpage__second-paraghraph loginpage__align-paragraph">Forgot password?</p>
+                <p class="loginpage__remind-password loginpage__align-left">Forgot password?</p>
                 <button class="loginpage__button" type="submit">Log in</button>
             </div>
         </form>
     </div>
 </div>
+</section>
+
  
 <?php
 get_footer();
