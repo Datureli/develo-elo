dcfc93bb8111f8af86a70d8913bd51a3b48c49dc
Datureli <soleris1@gmail.com>
2022-07-23 07:53:39 +0200

    remove unused code && fix few lint problems

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -2,22 +2,20 @@
 get_header();
 ?>
 
-<div class="loginpage__background">    
-     <div class="loginpage__logincontainer">
-        <h1 class="h1">Log in</h1>
-        <p>Log in with your data that you recieved from the photographer</p>
+<div class="loginpage__background">
+    <div class="loginpage__container">
+        <h1 class="loginpage__h1">Log in</h1>
+        <p class="loginpage_first-paragraph loginpage__align-paragraph">Log in with your data that you recieved from the photographer</p>
         <form>
-        <input type="text" placeholder="E-mail" class="input">
-        <input type="text" placeholder="Password" class="input">
-        <div class="loginpage__flexcontainer">
-        <p class="loginpage__paraghraph">Forgot password?</p>
-        <button class="loginpage__button" type="submit">Log in</button>
-        </div>
-
+            <input type="text" placeholder="E-mail" class="loginpage__input">
+            <input type="text" placeholder="Password" class="loginpage__input">
+            <div class="loginpage__flexcontainer">
+                <p class="loginpage__second-paraghraph loginpage__align-paragraph">Forgot password?</p>
+                <button class="loginpage__button" type="submit">Log in</button>
+            </div>
         </form>
-
-     </div>
     </div>
+</div>
  
 <?php
 get_footer();
