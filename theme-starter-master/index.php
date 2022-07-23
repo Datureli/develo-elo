1f907068ca8133ad6c6e8de78ddd7e1981827667
Datureli <soleris1@gmail.com>
2022-07-23 15:07:39 +0200

    fix indentation

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -1,23 +1,23 @@
 <?php
 get_header();
 ?>
+
 <div class="loginpage">
-<div class="loginpage__background">
-    <div class="loginpage__container">
-        <h1 class="loginpage__h1">Log in</h1>
-        <p class="loginpage_description loginpage__align-left">Log in with your data that you recieved from the photographer</p>
-        <form>
-            <input type="text" placeholder="E-mail" class="loginpage__input">
-            <input type="text" placeholder="Password" class="loginpage__input">
-            <div class="loginpage__flexcontainer">
-                <p class="loginpage__remind-password loginpage__align-left">Forgot password?</p>
-                <button class="loginpage__button" type="submit">Log in</button>
-            </div>
-        </form>
+    <div class="loginpage__background">
+        <div class="loginpage__container">
+            <h1 class="loginpage__h1">Log in</h1>
+            <p class="loginpage_description loginpage__align-left">Log in with your data that you recieved from the photographer</p>
+            <form>
+                <input type="text" placeholder="E-mail" class="loginpage__input">
+                <input type="text" placeholder="Password" class="loginpage__input">
+                <div class="loginpage__flexcontainer">
+                    <p class="loginpage__remind-password loginpage__align-left">Forgot password?</p>
+                    <button class="loginpage__button" type="submit">Log in</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
-</div>
 
- 
 <?php
 get_footer();
