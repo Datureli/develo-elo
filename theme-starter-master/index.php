5d24e8fafd8533fe6a0c22abf1d87d1c03edd5b1
Datureli <soleris1@gmail.com>
2022-07-25 09:05:32 +0200

    fix issues

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -6,12 +6,12 @@ get_header();
     <div class="loginpage__background">
         <div class="loginpage__container">
             <h1 class="loginpage__h1">Log in</h1>
-            <p class="loginpage_description loginpage__align-left">Log in with your data that you recieved from the photographer</p>
+            <p class="loginpage__paragraph">Log in with your data that you recieved from the photographer</p>
             <form>
                 <input type="text" placeholder="E-mail" class="loginpage__input">
                 <input type="text" placeholder="Password" class="loginpage__input">
                 <div class="loginpage__flexcontainer">
-                    <p class="loginpage__remind-password loginpage__align-left">Forgot password?</p>
+                    <p class="loginpage__paragraph loginpage__underline">Forgot password?</p>
                     <button class="loginpage__button" type="submit">Log in</button>
                 </div>
             </form>
